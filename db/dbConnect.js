d0e8ecafeecb6e71a27b6d422aad5bb981c394a0
algoroloro <algoroloro@gmail.com>
2022-07-26 18:28:32 +0200

    db url

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 async function dbConnect() {
-    mongoose.connect(process.env.DB_URL)
+    mongoose.connect('mongodb+srv://algorodev:Alg0r0D3v@algorodev.6hagy.mongodb.net/portfolio?retryWrites=true&w=majority')
 }
 
 module.exports = dbConnect
